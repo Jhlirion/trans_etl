9c0c08e9c7396b2e517aa0e27505555df7770d32
jhon <jhlirion@gmail.com>
2022-10-25 22:32:41 -0500

    tratamiento init

--- ETL/t_data.py
+++ ETL/t_data.py
@@ -4,3 +4,6 @@ import psycopg2.extras as extras
 import pandas as pd
 from datetime import date
 
+df_bibli = pd.read_csv(r'data\bibliotecas\2022-October\bibliotecas-12-10-2022.csv')
+df_cines = pd.read_csv(r'data\cines\2022-October\cines-12-10-2022.csv')
+df_museos = pd.read_csv(r'data\museos\2022-October\museos-12-10-2022.csv')
