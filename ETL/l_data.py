c240d75aff5a1073cf16d99da793fc17c290a9a9
jhon <jhlirion@gmail.com>
2022-11-09 20:32:43 -0500

    'carga'

--- ETL/l_data.py
+++ ETL/l_data.py
@@ -1,3 +1,18 @@
+from tkinter import E
+import sqlalchemy
+import psycopg2
+from sqlalchemy import create_engine
+import pandas as pd
 import t_data as tra     
 
-tra.df_bibli_final
+df = tra.df_bibli_final
+
+
+try:
+    conex = create_engine("postgresql+psycopg2://{}:{}@{}/{}".format("admin","admin","172.17.0.1:3333", "alkemy"))
+    conexpostgres = conex.connect()
+    metadatas = sqlalchemy.MetaData()
+    df.to_sql("df", conexpostgres, if_exists="append", index=False)
+    print("Vamo Loco")
+except Exception as e:
+    print(e)
